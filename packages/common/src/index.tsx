0ac530dfacb1592e6b06ec8b5f23b71e9623caeb
otkrickey <rtk.otk@gmail.com>
2021-04-12 12:08:11 +0900

    Initial commit

---
+++ packages/common/src/index.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class App extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1, backgroundColor: '#ffffff' }}>
+                <Text>Hello World</Text>
+            </View>
+        );
+    }
+}
+
+export default App;
---
+++ packages/web/src/App.tsx
@@ -0,0 +1,15 @@
+import App from '@otkrickey/common';
+export default App;
+// import React, { Component } from 'react';
+// import { Text, View } from 'react-native';
+
+// class App extends Component {
+//     render() {
+//         return (
+//             <View style={{ flex: 1, backgroundColor: '#ffffff' }}>
+//                 <Text>Hello World</Text>
+//             </View>
+//         );
+//     }
+// }
+// export default App;
